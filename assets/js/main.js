2fe1f544a62c2c3e8fdbc6896928de7a405a7139
wan <iwan.plamboyan@appschef.com>
2025-08-20 23:13:11 +0700

    update portfolio to version 2 in 20/08/2025

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 // Menu show Y hidden
-const header = document.querySelector('.header'),
-  navLogo = document.querySelector('.nav_logo'),
-  navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close'),
-  darkMode = document.getElementById('theme-button'),
-  toggleIcon = document.querySelector('.fa-th-large');
+const header = document.querySelector(".header"),
+  navLogo = document.querySelector(".nav_logo"),
+  navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close"),
+  darkMode = document.getElementById("theme-button"),
+  toggleIcon = document.querySelector(".fa-th-large");
 
 // Menu Show
 // Validate if constant exists
 if (navToggle) {
-  navToggle.addEventListener('click', function () {
-    navMenu.classList.toggle('show-menu');
-    darkMode.style.display = 'none';
-    toggleIcon.classList.remove('fa-th-large');
-    header.classList.add('transparent');
+  navToggle.addEventListener("click", function () {
+    navMenu.classList.toggle("show-menu");
+    darkMode.style.display = "none";
+    toggleIcon.classList.remove("fa-th-large");
+    header.classList.add("transparent");
     navLogo.style.opacity = 0;
   });
 }
@@ -22,49 +22,49 @@ if (navToggle) {
 // Menu Hidden
 // Validate if constant exist
 if (navClose) {
-  navClose.addEventListener('click', function () {
-    navMenu.classList.remove('show-menu');
-    darkMode.style.display = 'initial';
-    toggleIcon.classList.add('fa-th-large');
-    header.classList.remove('transparent');
+  navClose.addEventListener("click", function () {
+    navMenu.classList.remove("show-menu");
+    darkMode.style.display = "initial";
+    toggleIcon.classList.add("fa-th-large");
+    header.classList.remove("transparent");
     navLogo.style.opacity = 1;
   });
 }
 
 // Remove Menu Mobile
-const navLink = document.querySelectorAll('.nav_link');
+const navLink = document.querySelectorAll(".nav_link");
 
 function linkAction() {
-  const navMenu = document.getElementById('nav-menu');
+  const navMenu = document.getElementById("nav-menu");
   // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove('show-menu');
-  toggleIcon.classList.add('fa-th-large');
+  navMenu.classList.remove("show-menu");
+  toggleIcon.classList.add("fa-th-large");
 }
-navLink.forEach((n) => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // social-icons
-const socialIcons = document.querySelectorAll('.social-icon');
+const socialIcons = document.querySelectorAll(".social-icon");
 socialIcons.forEach((icon) => {
-  icon.addEventListener('mouseenter', function () {
-    icon.style.transition = '0.5s ease-in-out';
-    icon.style.transform = 'rotate(360deg)';
+  icon.addEventListener("mouseenter", function () {
+    icon.style.transition = "0.5s ease-in-out";
+    icon.style.transform = "rotate(360deg)";
     icon.style.color = icon.dataset.color;
-    icon.style.transformOrigin = 'center';
+    icon.style.transformOrigin = "center";
   });
 
-  icon.addEventListener('mouseout', function () {
-    icon.style.transition = '0.5s ease-in-out';
-    icon.style.transform = 'rotate(0deg)';
-    icon.style.color = '';
-    icon.style.transformOrigin = 'center';
+  icon.addEventListener("mouseout", function () {
+    icon.style.transition = "0.5s ease-in-out";
+    icon.style.transform = "rotate(0deg)";
+    icon.style.color = "";
+    icon.style.transformOrigin = "center";
   });
 });
 
 // Animation
-const home = document.querySelector('.home');
+const home = document.querySelector(".home");
 const move = 20;
-const animations = document.querySelectorAll('.animation');
-const profileImage = document.querySelector('.home_img');
+const animations = document.querySelectorAll(".animation");
+const profileImage = document.querySelector(".home_img");
 
 function movingElement(e) {
   const { offsetWidth: width, offsetHeight: height } = this;
@@ -74,7 +74,7 @@ function movingElement(e) {
   const yMove = Math.round((y / height) * move - move / 2);
 
   animations.forEach((animation) => {
-    if (animation.classList.contains('minus-float')) {
+    if (animation.classList.contains("minus-float")) {
       animation.style.transform = `translate(${-xMove}px, ${-yMove}px)`;
     } else {
       animation.style.transform = `translate(${xMove}px, ${yMove}px)`;
@@ -84,29 +84,29 @@ function movingElement(e) {
   profileImage.style.transform = `rotateY(${xMove}deg)rotateX(${-yMove}deg)`;
 }
 
-home.addEventListener('mousemove', movingElement);
+home.addEventListener("mousemove", movingElement);
 
 // Accordion skills
-const skillsContent = document.getElementsByClassName('skills_content'),
-  skillsHeader = document.querySelectorAll('.skills_header');
+const skillsContent = document.getElementsByClassName("skills_content"),
+  skillsHeader = document.querySelectorAll(".skills_header");
 
 skillsHeader.forEach((sh, index) => {
-  sh.addEventListener('click', function () {
+  sh.addEventListener("click", function () {
     if (index == 0) {
-      skillsContent[0].classList.toggle('skills_open');
-      skillsContent[0].classList.toggle('skills_close');
+      skillsContent[0].classList.toggle("skills_open");
+      skillsContent[0].classList.toggle("skills_close");
     } else if (index == 1) {
-      skillsContent[1].classList.toggle('skills_open');
-      skillsContent[1].classList.toggle('skills_close');
+      skillsContent[1].classList.toggle("skills_open");
+      skillsContent[1].classList.toggle("skills_close");
     } else if (index == 2) {
-      skillsContent[2].classList.toggle('skills_open');
-      skillsContent[2].classList.toggle('skills_close');
+      skillsContent[2].classList.toggle("skills_open");
+      skillsContent[2].classList.toggle("skills_close");
     }
   });
 });
 
 // swipper sertifikat
-var swiper = new Swiper('.mySwiper', {
+var swiper = new Swiper(".mySwiper", {
   loop: true,
   cssMode: true,
   spaceBetween: 30,
@@ -117,25 +117,17 @@ var swiper = new Swiper('.mySwiper', {
     disableOnInteraction: false,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
-// alert_close
-const alert = document.querySelector('.alert');
-const alertClose = document.querySelector('.alert_close');
-
-alertClose.addEventListener('click', function () {
-  alert.classList.add('d-none');
-});
-
 // Scroll Sections Active Link
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
   const scrollY = window.pageYOffset;
@@ -143,85 +135,76 @@ function scrollActive() {
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute('id');
+    sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
     } else {
-      document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
     }
   });
 }
-window.addEventListener('scroll', scrollActive);
+window.addEventListener("scroll", scrollActive);
 
 // Change Background Header
 function scrollHeader() {
-  const nav = document.getElementById('header');
+  const nav = document.getElementById("header");
   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 80) nav.classList.add('scroll-header');
-  else nav.classList.remove('scroll-header');
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener("scroll", scrollHeader);
 
 // Show Scroll Up
 function scrollUp() {
-  const scrollUp = document.getElementById('scroll-up');
+  const scrollUp = document.getElementById("scroll-up");
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
-  else scrollUp.classList.remove('show-scroll');
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp);
+window.addEventListener("scroll", scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'fa-sun';
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "fa-sun";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
-const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun');
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "fa-moon" : "fa-sun";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-  themeButton.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove'](iconTheme);
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "fa-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener("click", () => {
   // Add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
   // We save the theme and the current icon that the user chose
-  localStorage.setItem('selected-theme', getCurrentTheme());
-  localStorage.setItem('selected-icon', getCurrentIcon());
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
-// My Google Script/App Script
-
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwjkoudFYxNBpAQLNMrGJmarAHLscflJMnTp-r7yfQKuMXxK3IR7KYBpJCQNfzLWiGweQ/exec';
-const form = document.forms['my-portfolio'];
-const btnKirim = document.querySelector('.btn-kirim');
-const btnLoading = document.querySelector('.btn-loading');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // ketika tombol submit diClick
-  btnLoading.classList.toggle('d-none');
-  btnKirim.classList.toggle('d-none');
-
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then((response) => {
-      console.log('Success!', response);
-      btnLoading.classList.toggle('d-none');
-      btnKirim.classList.toggle('d-none');
-      alert.classList.remove('d-none');
-      form.reset();
-    })
-    .catch((error) => console.error('Error!', error.message));
+// AOS
+AOS.init({
+  once: true,
 });
